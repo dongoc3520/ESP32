24318edd613dc8d7a499a772d5ae8743e6a0a7e5
dongoc09092002 <ngocdo09092002@gmail.com>
2023-06-15 12:11:28 +0700

    first commit

---
+++ main/pwm.c
@@ -0,0 +1,439 @@
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "protocol_examples_common.h"
+#include "esp_tls.h"
+
+
+#include <stdio.h>
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "driver/gpio.h"
+#include <stdlib.h>
+
+#include "sdkconfig.h"
+#include "esp_spi_flash.h"
+
+
+
+// esp htpp client
+
+
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+
+#define EXAMPLE_ESP_WIFI_SSID      "Galaxy Note9"
+#define EXAMPLE_ESP_WIFI_PASS      "12345678"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+
+// handler client
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            if (output_buffer != NULL) {
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+    }
+    return ESP_OK;
+}
+
+ esp_http_client_config_t config = {
+        // .url = "http://192.168.0.75:5000/create?nhietdo=80&doam=185",
+        .url = "",
+        .event_handler=_http_event_handler
+    };
+
+static void http_native_request(int nhietdo1 , int doam1 ,esp_http_client_config_t config)
+{
+    char output_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};   // Buffer to store response of http request
+    int content_length = 0;
+            char str1[100] = "http://192.168.43.137:5000/create?nhietdo=";
+            char nhietdo[3] ;
+            sprintf(nhietdo, "%d", nhietdo1);
+        char str2[] = "&doam=";
+                    char doam[4];
+        sprintf(doam, "%d", doam1);
+        strcat(str1, nhietdo);
+        strcat(str1, str2);
+        strcat(str1, doam);
+        config.url = str1;
+        
+    // esp_http_client_config_t config = {
+    //     // .url = "http://192.168.0.75:5000/create?nhietdo=80&doam=185",
+
+    //     .url = str1,
+    //     .event_handler=_http_event_handler
+    // };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET Request
+    esp_http_client_set_method(client, HTTP_METHOD_GET);
+    // test
+    // while(1){
+        // vTaskDelay(1000 / portTICK_RATE_MS);
+    esp_err_t err = esp_http_client_open(client, 0);
+        
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+    } else {
+        content_length = esp_http_client_fetch_headers(client);
+        if (content_length < 0) {
+            ESP_LOGE(TAG, "HTTP client fetch headers failed");
+        } else {
+            int data_read = esp_http_client_read_response(client, output_buffer, MAX_HTTP_OUTPUT_BUFFER);
+            if (data_read >= 0) {
+                output_buffer[data_read] = '\0';
+                ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+                ESP_LOGI(TAG, "Response: %s", output_buffer);
+            } else {
+                ESP_LOGE(TAG, "Failed to read response");
+            }
+        }
+    }
+    
+ 
+    esp_http_client_cleanup(client);
+	vTaskDelete(NULL);
+    esp_http_client_close(client);}
+
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+           
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+
+// dht11
+enum dht11_status {
+    DHT11_CRC_ERROR = -2,
+    DHT11_TIMEOUT_ERROR,
+    DHT11_OK
+};
+
+struct dht11_reading {
+    int status;
+    int temperature;
+    int humidity;
+};
+
+static gpio_num_t dht_gpio;
+
+
+static int64_t last_read_time = -2000000;
+static struct dht11_reading last_read;
+
+static int _waitOrTimeout(uint16_t microSeconds, int level) {
+    int micros_ticks = 0;
+    while(gpio_get_level(dht_gpio) == level) { 
+        if(micros_ticks++ > microSeconds) 
+            return DHT11_TIMEOUT_ERROR;
+        ets_delay_us(1);
+    }
+    return micros_ticks;
+}
+
+static int _checkCRC(uint8_t data[]) {
+    if(data[4] == (data[0] + data[1] + data[2] + data[3]))
+        return DHT11_OK;
+    else
+        return DHT11_CRC_ERROR;
+}
+
+static void _sendStartSignal() {
+    gpio_set_direction(dht_gpio, GPIO_MODE_OUTPUT);
+    gpio_set_level(dht_gpio, 0);
+    ets_delay_us(20 * 1000);
+    gpio_set_level(dht_gpio, 1);
+    ets_delay_us(40);
+    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+}
+
+static int _checkResponse() {
+    /* Wait for next step ~80us*/
+    if(_waitOrTimeout(80, 0) == DHT11_TIMEOUT_ERROR)
+        return DHT11_TIMEOUT_ERROR;
+
+    /* Wait for next step ~80us*/
+    if(_waitOrTimeout(80, 1) == DHT11_TIMEOUT_ERROR) 
+        return DHT11_TIMEOUT_ERROR;
+
+    return DHT11_OK;
+}
+
+static struct dht11_reading _timeoutError() {
+    struct dht11_reading timeoutError = {DHT11_TIMEOUT_ERROR, -1, -1};
+    return timeoutError;
+}
+
+static struct dht11_reading _crcError() {
+    struct dht11_reading crcError = {DHT11_CRC_ERROR, -1, -1};
+    return crcError;
+}
+
+void DHT11_init(gpio_num_t gpio_num) {
+    /* Wait 1 seconds to make the device pass its initial unstable status */
+    vTaskDelay(1000 / 1);
+    dht_gpio = gpio_num;
+}
+
+struct dht11_reading DHT11_read() {
+    /* Tried to sense too son since last read (dht11 needs ~2 seconds to make a new read) */
+    if(esp_timer_get_time() - 2000000 < last_read_time) {
+        return last_read;
+    }
+
+    last_read_time = esp_timer_get_time();
+
+    uint8_t data[5] = {0,0,0,0,0};
+
+    _sendStartSignal();
+
+    if(_checkResponse() == DHT11_TIMEOUT_ERROR)
+        return last_read = _timeoutError();
+    
+    /* Read response */
+    for(int i = 0; i < 40; i++) {
+        /* Initial data */
+        if(_waitOrTimeout(50, 0) == DHT11_TIMEOUT_ERROR)
+            return last_read = _timeoutError();
+                
+        if(_waitOrTimeout(70, 1) > 28) {
+            /* Bit received was a 1 */
+            data[i/8] |= (1 << (7-(i%8)));
+        }
+    }
+
+    if(_checkCRC(data) != DHT11_CRC_ERROR) {
+        last_read.status = DHT11_OK;
+        last_read.temperature = data[2];
+        last_read.humidity = data[0];
+        return last_read;
+    } else {
+        return last_read = _crcError();
+    }
+}
+
+
+
+
+void app_main(void)
+
+
+
+
+{
+    
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+        wifi_init_sta();
+        esp_netif_init();
+
+esp_event_loop_create_default();
+//    int a =  DHT11_read().temperature;
+//     int b = DHT11_read().humidity;
+     while(1){
+    DHT11_init(GPIO_NUM_4);
+    int a =  DHT11_read().temperature;
+    int b = DHT11_read().humidity;
+    printf("Temperature is %d \n", DHT11_read().temperature);
+     printf("Humidity is %d\n", DHT11_read().humidity);
+    printf("Status code is %d\n", DHT11_read().status);
+
+    http_native_request(a ,b , config);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+
+//  http_native_request(a ,b);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
